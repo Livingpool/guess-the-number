d2a6579ddf23e9f1a38885b52a297790fd71f765
Livingpool <52132459+Livingpool@users.noreply.github.com>
2024-05-26 21:49:05 +0800

    modify polling

--- polling/queue.go
+++ polling/queue.go
@@ -3,15 +3,16 @@ package polling
 import "sync"
 
 type Queue[T any] struct {
-	items []T
-	lock  sync.RWMutex
-	cap   int
+	items    []T
+	lock     *sync.RWMutex
+	capacity int
 }
 
-func NewQueue[T any](cap int) *Queue[T] {
+func NewQueue[T any](capacity int) *Queue[T] {
 	return &Queue[T]{
-		items: make([]T, 0, cap),
-		cap:   cap,
+		items:    make([]T, 0, capacity),
+		lock:     new(sync.RWMutex),
+		capacity: capacity,
 	}
 }
 
@@ -19,18 +20,19 @@ func (q *Queue[T]) Enqueue(item T) {
 	q.lock.Lock()
 	defer q.lock.Unlock()
 
-	// If queue is at capacity, remove the oldest item.
-	if len(q.items) == q.cap {
+	// If queue is at capacity, dequeue oldest item
+	if len(q.items) == q.capacity {
 		q.items = q.items[1:]
 	}
 
+	// Enqueue new item
 	q.items = append(q.items, item)
 }
 
+// Return a copy of q.items
 func (q *Queue[T]) Copy() []T {
 	q.lock.RLock()
 	defer q.lock.RUnlock()
 
-	// Return a copy of the slice
 	return append([]T(nil), q.items...)
 }
